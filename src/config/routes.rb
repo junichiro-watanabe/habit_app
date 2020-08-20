e275b67b3ce8b6b065beef9b8e2f1d5dc8ca41a8
Your Name <junichiro_watanabe@watanavi.com>
2020-08-21 00:09:33 +0900

    ホーム/サインアップ画面を作成

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/sign_up', to: 'users#new'
+  root 'static_pages#home'
+  resources :users
 end
