9dd57848c057e51d38cc7fb0a1c1085d6b6d1de2
Your Name <junichiro.watanabe.1113@gmail.com>
2020-09-10 02:11:20 +0900

    サイドバーをリファクタリング

---
+++ src/app/javascript/components/Sidebar.js
@@ -0,0 +1,22 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <aside class="profile">
+          <div class="user-info">
+            <img src={this.props.user_image} />
+            <h4>{this.props.user_name}</h4>
+          </div>
+
+          <ul>
+            {this.props.links.map((item) => <li><a href={item.href}>{item.link}</a></li>)}
+          </ul>
+        </aside>
+      </React.Fragment >
+    );
+  }
+}
+
+export default Sidebar
