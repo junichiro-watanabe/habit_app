b10bf5ec8d54c4c8c87182834d817bfcdff0b9c3
Your Name <junichiro.watanabe.1113@gmail.com>
2020-09-13 04:26:18 +0900

    エラーメッセージ出力を修正

--- src/spec/helpers/groups_helper_spec.rb
+++ src/spec/helpers/groups_helper_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the GroupHelper. For example:
-#
-# describe GroupHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe GroupsHelper, type: :helper do
+  describe "" do
+    it "無効なコミュニティ名が入力された場合のエラー出力" do
+      errors = ["Name can't be blank"]
+      expect(group_error_messages(errors).include?("コミュニティ名は空欄にできません")).to eq true
+      errors = ["Name is too long (maximum is 50 characters)"]
+      expect(group_error_messages(errors).include?("コミュニティ名は50文字以下にしてください")).to eq true
+    end
+
+    it "無効な習慣が入力された場合のエラー出力" do
+      errors = ["Habit can't be blank"]
+      expect(group_error_messages(errors).include?("習慣は空欄にできません")).to eq true
+      errors = ["Habit is too long (maximum is 50 characters)"]
+      expect(group_error_messages(errors).include?("習慣は50文字以下にしてください")).to eq true
+    end
+
+    it "無効な概要が入力された場合のエラー出力" do
+      errors = ["Overview can't be blank"]
+      expect(group_error_messages(errors).include?("概要は空欄にできません")).to eq true
+      errors = ["Overview is too long (maximum is 255 characters)"]
+      expect(group_error_messages(errors).include?("概要は255文字以下にしてください")).to eq true
+    end
+  end
 end
--- src/spec/helpers/users_helper_spec.rb
+++ src/spec/helpers/users_helper_spec.rb
@@ -1,40 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe UsersHelper, type: :helper do
-  include UsersHelper
-
   describe "user_error_messagesのテスト"
     it "無効な名前が入力された場合のエラー出力" do
       errors = ["Name can't be blank"]
-      error = user_error_messages(errors)
-      expect(error.include?("名前は空白にできません")).to eq true
+      expect(user_error_messages(errors).include?("名前は空白にできません")).to eq true
 
       errors = ["Name is too long (maximum is 50 characters)"]
-      error = user_error_messages(errors)
-      expect(error.include?("名前は50文字以下にしてください")).to eq true
+      expect(user_error_messages(errors).include?("名前は50文字以下にしてください")).to eq true
     end
 
     it "無効な名前が入力された場合のエラー出力" do
       errors = ["Email can't be blank", "Email is invalid"]
-      error = user_error_messages(errors)
-      expect(error.include?("メールアドレスは空白にできません")).to eq true
+      expect(user_error_messages(errors).include?("メールアドレスは空白にできません")).to eq true
       errors = ["Email has already been taken"]
-      error = user_error_messages(errors)
-      expect(error.include?("既にそのメールアドレスは使用されています")).to eq true
+      expect(user_error_messages(errors).include?("既にそのメールアドレスは使用されています")).to eq true
       errors = ["Email is too long (maximum is 255 characters)"]
-      error = user_error_messages(errors)
-      expect(error.include?("メールアドレスは255文字以下にしてください")).to eq true
+      expect(user_error_messages(errors).include?("メールアドレスは255文字以下にしてください")).to eq true
       errors = ["Email is invalid"]
-      error = user_error_messages(errors)
-      expect(error.include?("メールアドレスが不正な値です")).to eq true
+      expect(user_error_messages(errors).include?("メールアドレスが不正な値です")).to eq true
     end
 
     it "無効なパスワードが入力された場合のエラー出力" do
       errors = ["Password can't be blank"]
-      error = user_error_messages(errors)
-      expect(error.include?("パスワードは空白にできません")).to eq true
+      expect(user_error_messages(errors).include?("パスワードは空白にできません")).to eq true
       errors = ["Password is too short (minimum is 8 characters)"]
-      error = user_error_messages(errors)
-      expect(error.include?("パスワードは8文字以上に設定してください")).to eq true
+      expect(user_error_messages(errors).include?("パスワードは8文字以上に設定してください")).to eq true
     end
 end
