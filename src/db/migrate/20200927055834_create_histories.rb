af7757dfd4db61c0de34f11d3c4d535f933c3f30
Your Name <junichiro_watanabe@watanavi.com>
2020-09-27 17:15:38 +0900

    目標達成機能追加

---
+++ src/db/migrate/20200927055834_create_histories.rb
@@ -0,0 +1,12 @@
+class CreateHistories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :histories do |t|
+      t.references :achievement, null: false, foreign_key: true
+      t.date :date
+
+      t.timestamps
+    end
+    add_index :histories, :date
+    add_index :histories, [:achievement_id, :date]
+  end
+end
