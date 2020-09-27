2adfa18787c26b8a10fb0fec39821fbe544b1a15
Your Name <junichiro.watanabe.1113@gmail.com>
2020-09-27 16:22:24 +0900

    テスト追加

--- src/spec/requests/achievement_request_spec.rb
+++ src/spec/requests/achievement_request_spec.rb
@@ -18,8 +18,8 @@ RSpec.describe "Achievements", type: :request do
       log_in_as(@user_1)
       expect(logged_in?).to eq true
       expect(@user_1.achieved?(@group)).to eq false
-      expect{ patch achievement_path(@group) }.to change{ @user_1.achieved?(@group) }.to(true)
-      expect{ patch achievement_path(@group) }.to change{ @user_1.achieved?(@group) }.to(false)
+      expect{ patch achievement_path(@group) }.to change{ @user_1.achieved?(@group) }.to(true).and change{ @user_1.microposts.count }.by(+1)
+      expect{ patch achievement_path(@group) }.to change{ @user_1.achieved?(@group) }.to(false).and change{ @user_1.microposts.count }.by(-1)
     end
 
     it "達成状況の切り替え：ログインしていない" do
@@ -30,7 +30,9 @@ RSpec.describe "Achievements", type: :request do
     end
 
     it "達成状況の切り替え：所属していない" do
-
+      log_in_as(@user_2)
+      expect{ patch achievement_path(@group) }.not_to change{ @user_2.achieved?(@group) }
+      expect(response).to redirect_to groups_path
     end
   end
 end
