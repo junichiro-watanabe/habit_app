b8500710fd5368776e6258430d858d887d8091ba
Your Name <junichiro.watanabe.1113@gmail.com>
2020-10-02 00:34:04 +0900

    グループ説明画面編集

--- src/app/controllers/belongs_controller.rb
+++ src/app/controllers/belongs_controller.rb
@@ -8,13 +8,14 @@ class BelongsController < ApplicationController
     response = Belong.where(user: current_user).find_by(group: group)
     response = response.attributes
     response.store("member_count", group.members.count)
+    response.store("achieved", current_user.achieved?(group))
     render json: response
   end
 
   def destroy
     group = Group.find(params[:id])
     current_user.leave(group)
-    response = {member_count: group.members.count}
+    response = {member_count: group.members.count, achieved: false}
     render json: response
   end
 end
