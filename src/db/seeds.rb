db92b1a3f68f1ad5897b6e72251744464c44b20a
Your Name <junichiro.watanabe.1113@gmail.com>
2020-10-04 18:33:04 +0900

    サンプルデータ追加

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -9,18 +9,28 @@
 Faker::Config.locale = :ja
 srand(0)
 
+users_json = []
+groups_json = []
+File.open("db/fixtures/users.json") do |j|
+  users_json = JSON.load(j)
+end
+File.open("db/fixtures/groups.json") do |j|
+  groups_json = JSON.load(j)
+end
+
 User.create!(name: "ゲストユーザ",
              email: "guest@example.com",
+             introduction: "一時利用のためのゲストユーザです。よろしくお願いします。",
              password: "password",
              password_confirmation: "password")
 
-99.times do |n|
+(users_json.length).times do |n|
   user = User.create!(name: Faker::Name.name,
-                     email: "test#{n}@example.com",
-                     introduction: Faker::Lorem.sentence,
+                     email: "test#{n+1}@example.com",
+                     introduction: users_json[n]["introduction"],
                      password: "password",
                      password_confirmation: "password")
-  if rand(2) == 0 || rand(2) == 1
+  if rand(3) != 3
     user.image.attach(io: File.open("db/fixtures/images/image (#{rand(200)}).png"), filename: "image (#{rand(200)}).png")
   end
   user.save
@@ -28,32 +38,32 @@ end
 
 2.times do |n|
   user = User.find(1)
-  group = user.groups.build(name: Faker::Team.name,
-                            habit: Faker::Job.title,
-                            overview: Faker::Lorem.sentence)
-  if rand(2) == 0 || rand(2) == 1
+  group = user.groups.build(name: "テストグループ #{n + 1}",
+                            habit: "毎日habit appを起動しよう！",
+                            overview: "ゲストユーザが作成したテストグループです。毎日habitappを起動して習慣づけを頑張りましょう！")
+  if rand(3) != 3
     group.image.attach(io: File.open("db/fixtures/images/image (#{rand(200)}).png"), filename: "image (#{rand(200)}).png")
   end
   group.save
   user.belong(group)
 end
 
-48.times do |n|
-  user = User.find(rand(1..100))
-  group = user.groups.build(name: Faker::Team.name,
-                            habit: Faker::Job.title,
-                            overview: Faker::Lorem.sentence)
-  if rand(2) == 0 || rand(2) == 1
+(groups_json.length).times do |n|
+  user = User.find(rand(1..User.count))
+  group = user.groups.build(name: groups_json[n]["name"],
+                            habit: groups_json[n]["habit"],
+                            overview: groups_json[n]["overview"])
+  if rand(3) != 3
     group.image.attach(io: File.open("db/fixtures/images/image (#{rand(200)}).png"), filename: "image (#{rand(200)}).png")
   end
   group.save
   user.belong(group)
 end
 
-100.times do |n|
+User.count.times do |n|
   user = User.find(n + 1)
-  1.upto 10 do |m|
-    group = Group.find(rand(1..50))
+  3.times do |m|
+    group = Group.find(rand(1..Group.count))
     user.belong(group) until user.belonging?(group)
   end
 end
