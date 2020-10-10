0ceaeae857434690e0d8451e93679b577c5c1c80
Your Name <junichiro.watanabe.1113@gmail.com>
2020-10-11 03:18:47 +0900

    検索機能のテスト追加

--- src/spec/factories/users.rb
+++ src/spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :guest, class: User do
     name { "ゲストユーザ" }
     email { "guest@example.com" }
-    introduction { "introduction" }
+    introduction { "guest_introduction" }
     password { "password" }
     password_confirmation { "password" }
   end
@@ -10,7 +10,7 @@ FactoryBot.define do
   factory :user, class: User do
     name { "user" }
     email { "user@example.com" }
-    introduction { "introduction" }
+    introduction { "user_introduction" }
     password { "password" }
     password_confirmation { "password" }
   end
@@ -18,15 +18,15 @@ FactoryBot.define do
   factory :other_user, class: User do
     name { "other_user" }
     email { "other_user@example.com" }
-    introduction { "introduction" }
+    introduction { "other_user_introduction" }
     password { "password" }
     password_confirmation { "password" }
   end
 
   factory :users, class: User do
-    sequence(:name, 1) { |n| "user #{n}" }
-    sequence(:email, 1) { |n| "user_#{n}@example.com"}
-    introduction { "introduction" }
+    sequence(:name, 2) { |n| "user #{n}" }
+    sequence(:email, 2) { |n| "user_#{n}@example.com"}
+    sequence(:introduction, 2) { |n| "user_#{n}_introduction" }
     password { "password" }
     password_confirmation { "password" }
   end
