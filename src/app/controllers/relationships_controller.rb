569653608b294eff1706fa4575a8365007d1e7bb
Your Name <junichiro_watanabe@watanavi.com>
2020-10-12 00:42:59 +0900

    ユーザ照会画面を編集

--- src/app/controllers/relationships_controller.rb
+++ src/app/controllers/relationships_controller.rb
@@ -5,20 +5,20 @@ class RelationshipsController < ApplicationController
   def update
     user = User.find(params[:id])
     current_user.follow(user)
-    response = {active_user_following_count: current_user.following.count,
-                active_user_followers_count: current_user.followers.count,
-                passive_user_following_count: user.following.count,
-                passive_user_followers_count: user.followers.count}
+    response = {active_following_count: current_user.following.count,
+                active_followers_count: current_user.followers.count,
+                passive_following_count: user.following.count,
+                passive_followers_count: user.followers.count}
     render json: response
   end
 
   def destroy
     user = User.find(params[:id])
     current_user.unfollow(user)
-    response = {active_user_following_count: current_user.following.count,
-                active_user_followers_count: current_user.followers.count,
-                passive_user_following_count: user.following.count,
-                passive_user_followers_count: user.followers.count}
+    response = {active_following_count: current_user.following.count,
+                active_followers_count: current_user.followers.count,
+                passive_following_count: user.following.count,
+                passive_followers_count: user.followers.count}
     render json: response
   end
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :users
   resources :users do
     member do
-      get :edit_image, :delete, :owning, :belonging, :not_achieved, :encouraged
+      get :edit_image, :delete, :owning, :belonging, :not_achieved, :encouraged, :following, :followers
     end
   end
   get '/create_group', to: 'groups#new'
