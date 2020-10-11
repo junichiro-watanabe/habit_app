7cb1ea09e6cf5ad1a0c68ce4ceb2bba40759b14e
Your Name <junichiro_watanabe@watanavi.com>
2020-10-11 23:04:37 +0900

    フォロー機能を追加

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -20,11 +20,12 @@ Rails.application.routes.draw do
       get :edit_image, :delete, :member
     end
   end
-  resources :belongs, only: [:update, :destroy, :show]
+  resources :belongs, only: [:update, :destroy]
   resources :achievements, only: [:update]
   resources :achievements do
     member do
       post :encourage
     end
   end
+  resources :relationships, only: [:update, :destroy]
 end
