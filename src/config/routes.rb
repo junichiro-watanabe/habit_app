5e7f27dcb92d5c4a06fa42e2e56f06ff36d80877
Your Name <junichiro_watanabe@watanavi.com>
2020-10-21 02:09:45 +0900

    テスト修正

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'group/edit'
   root 'static_pages#home'
   get '/signup', to: 'users#new'
+  get '/login', to: 'static_pages#home'
   post '/login', to: 'sessions#create'
   delete '/login', to: 'sessions#destroy'
   get '/login_guest', to: 'sessions#create_guest'
