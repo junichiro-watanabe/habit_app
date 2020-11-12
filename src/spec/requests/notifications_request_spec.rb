17fe63da5773468105d614454000f27f730ec3cf
Your Name <junichiro.watanabe.1113@gmail.com>
2020-11-12 22:35:21 +0900

    お知らせ機能：systemテスト追加

--- src/spec/requests/notifications_request_spec.rb
+++ src/spec/requests/notifications_request_spec.rb
@@ -21,12 +21,12 @@ RSpec.describe "Notifications", type: :request do
       log_in_as(@user)
       expect(logged_in?).to eq true
       expect(@user.passive_notifications.where(checked: false).count).to eq 3
-      expect{ patch notification_path(@user) }.to change{ @user.passive_notifications.where(checked: false).count }.to(0)
+      expect { patch notification_path(@user) }.to change { @user.passive_notifications.where(checked: false).count }.to(0)
     end
 
     it "お知らせ確認：ログインしていない" do
       expect(@user.passive_notifications.where(checked: false).count).to eq 3
-      expect{ patch notification_path(@user) }.not_to change{ @user.passive_notifications.where(checked: false).count }
+      expect { patch notification_path(@user) }.not_to change { @user.passive_notifications.where(checked: false).count }
       expect(response).to redirect_to root_path
       expect(flash.any?).to eq true
     end
@@ -35,7 +35,7 @@ RSpec.describe "Notifications", type: :request do
       log_in_as(@other_user)
       expect(logged_in?).to eq true
       expect(@user.passive_notifications.where(checked: false).count).to eq 3
-      expect{ patch notification_path(@user) }.not_to change{ @user.passive_notifications.where(checked: false).count }
+      expect { patch notification_path(@user) }.not_to change { @user.passive_notifications.where(checked: false).count }
       expect(response).to redirect_to root_path
     end
   end
