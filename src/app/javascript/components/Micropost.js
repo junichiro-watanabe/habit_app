902602e4b5b002bf62c119003c28cbb760b8cc89
Your Name <junichiro_watanabe@watanavi.com>
2020-11-14 02:37:19 +0900

    micropostにフックを使用

--- src/app/javascript/components/Micropost.js
+++ src/app/javascript/components/Micropost.js
@@ -1,60 +1,56 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Like from "./Like"
-class Micropost extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  getClass() {
-    if (this.props.encouragement) {
+function Micropost(props) {
+
+  function getClass() {
+    if (props.encouragement) {
       return "micropost encouragement-micropost";
     } else {
       return "micropost";
     }
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <div className={this.getClass()}>
-          <div className="micropost-user">
-            <img src={this.props.user_image} />
-            <div>
-              {this.props.encouragement ?
-                <React.Fragment>
-                  <h4>
-                    <span className="glyphicon glyphicon-fire" aria-hidden="true"></span>
-                    &nbsp;&nbsp;<a href={this.props.group_path}>{this.props.group_name}</a>
-                    &nbsp;の&nbsp;<a href={this.props.user_path}>{this.props.user_name}</a>
+  return (
+    <React.Fragment>
+      <div className={getClass()}>
+        <div className="micropost-user">
+          <img src={props.user_image} />
+          <div>
+            {props.encouragement ?
+              <React.Fragment>
+                <h4>
+                  <span className="glyphicon glyphicon-fire" aria-hidden="true"></span>
+                    &nbsp;&nbsp;<a href={props.group_path}>{props.group_name}</a>
+                    &nbsp;の&nbsp;<a href={props.user_path}>{props.user_name}</a>
                     &nbsp;さんが煽っています
                     &nbsp;&nbsp;<span className="glyphicon glyphicon-fire" aria-hidden="true"></span>
-                  </h4>
-                </React.Fragment> :
-                <a href={this.props.user_path}><li><h4>{this.props.user_name}</h4></li></a>}
-              <div className="micropost-time">
-                <p>{this.props.time}</p>
-                <Like
-                  path={this.props.like_path}
-                  like={this.props.like}
-                  like_count={this.props.like_count}
-                  token={this.props.token} />
-              </div>
+                </h4>
+              </React.Fragment> :
+              <a href={props.user_path}><li><h4>{props.user_name}</h4></li></a>}
+            <div className="micropost-time">
+              <p>{props.time}</p>
+              <Like
+                path={props.like_path}
+                like={props.like}
+                like_count={props.like_count}
+                token={props.token} />
             </div>
-            {this.props.poster ?
-              <a className="remove" rel="nofollow" data-method="delete" href={this.props.micropost_path}>
-                <span className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
-              </a> : null}
           </div>
-          <div className="micropost-content">
-            {this.props.history && !this.props.encouragement ?
-              <p dangerouslySetInnerHTML={{ __html: this.props.content }} /> :
-              <p>{this.props.content}</p>}
-          </div>
-        </div >
-      </React.Fragment >
-    );
-  }
+          {props.poster ?
+            <a className="remove" rel="nofollow" data-method="delete" href={props.micropost_path}>
+              <span className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
+            </a> : null}
+        </div>
+        <div className="micropost-content">
+          {props.history && !props.encouragement ?
+            <p dangerouslySetInnerHTML={{ __html: props.content }} /> :
+            <p>{props.content}</p>}
+        </div>
+      </div >
+    </React.Fragment >
+  );
 }
 
 Micropost.proptypes = {
