ccc23fcb199af948c2c7ce20b64b2c6655361aec
Your Name <junichiro.watanabe.1113@gmail.com>
2020-11-27 23:31:50 +0900

    目標達成ボタンにフックを使用

--- src/app/javascript/components/Achievement.js
+++ src/app/javascript/components/Achievement.js
@@ -1,32 +1,28 @@
 import React from "react"
 import PropTypes from "prop-types"
-class Achievement extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  toggleAchieve = () => {
-    fetch(this.props.path, {
+function Achievement(props) {
+
+  function toggleAchieve() {
+    fetch(props.path, {
       method: 'PATCH',
       headers: new Headers({ "Content-type": "application/json" }),
-      body: JSON.stringify({ "authenticity_token": this.props.token })
+      body: JSON.stringify({ "authenticity_token": props.token })
     }).then((response) => response.json()
     ).then(
       (json) => {
-        this.props.setAchieved(json.achieved)
+        props.setAchieved(json.achieved)
       }
     )
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <button className="btn btn-primary" onClick={this.toggleAchieve}>
-          達成状況の変更
-        </button>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <button className="btn btn-primary" onClick={toggleAchieve}>
+        達成状況の変更
+      </button>
+    </React.Fragment>
+  );
 }
 
 Achievement.PropTypes = {
