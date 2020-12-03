c5089627e3527cafc4b6d2455bf6cd8460fecca6
Your Name <junichiro.watanabe.1113@gmail.com>
2020-12-03 22:33:32 +0900

    フォローボタンにフックを使用

--- src/app/javascript/components/Follow.js
+++ src/app/javascript/components/Follow.js
@@ -1,20 +1,18 @@
 import React from "react"
 import PropTypes from "prop-types"
-class Follow extends React.Component {
-  constructor(props) {
-    super(props)
-  }
 
-  follow = () => {
-    fetch(this.props.path, {
+function Follow(props) {
+
+  function follow() {
+    fetch(props.path, {
       method: 'PATCH',
       headers: new Headers({ "Content-type": "application/json" }),
-      body: JSON.stringify({ "authenticity_token": this.props.token })
+      body: JSON.stringify({ "authenticity_token": props.token })
     }).then((response) => response.json()
     ).then(
       (json) => {
-        this.props.setFollow(json.follow)
-        this.props.setFollowCount(
+        props.setFollow(json.follow)
+        props.setFollowCount(
           json.active_following_count,
           json.active_followers_count,
           json.passive_following_count,
@@ -23,16 +21,16 @@ class Follow extends React.Component {
     )
   }
 
-  unfollow = () => {
-    fetch(this.props.path, {
+  function unfollow() {
+    fetch(props.path, {
       method: 'DELETE',
       headers: new Headers({ "Content-type": "application/json" }),
-      body: JSON.stringify({ "authenticity_token": this.props.token })
+      body: JSON.stringify({ "authenticity_token": props.token })
     }).then((response) => response.json()
     ).then(
       (json) => {
-        this.props.setFollow(json.follow)
-        this.props.setFollowCount(
+        props.setFollow(json.follow)
+        props.setFollowCount(
           json.active_following_count,
           json.active_followers_count,
           json.passive_following_count,
@@ -41,23 +39,21 @@ class Follow extends React.Component {
     )
   }
 
-  getClass() {
-    if (this.props.follow) {
+  function getClass() {
+    if (props.follow) {
       return "btn btn-warning";
     } else {
       return "btn btn-default";
     }
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <button className={this.getClass()} onClick={this.props.follow ? this.unfollow : this.follow}>
-          {this.props.follow ? "フォローを外す" : "フォローする"}
-        </button>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <button className={getClass()} onClick={props.follow ? unfollow : follow}>
+        {props.follow ? "フォローを外す" : "フォローする"}
+      </button>
+    </React.Fragment>
+  );
 }
 
 Follow.PropTypes = {
