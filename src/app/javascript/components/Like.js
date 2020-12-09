11bc3bb298054d9c63262047c6653ace69f20f32
Your Name <junichiro.watanabe.1113@gmail.com>
2020-12-09 12:07:37 +0900

    いいねボタンにフックを使用

--- src/app/javascript/components/Like.js
+++ src/app/javascript/components/Like.js
@@ -1,64 +1,54 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
-class Like extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      like: this.props.like,
-      likeCount: this.props.like_count
-    }
-  }
 
-  like = () => {
-    fetch(this.props.path, {
+function Like(props) {
+  const [liked, setLiked] = useState(props.like)
+  const [likeCount, setLikeCount] = useState(props.like_count)
+
+  function like() {
+    fetch(props.path, {
       method: 'PATCH',
       headers: new Headers({ "Content-type": "application/json" }),
-      body: JSON.stringify({ "authenticity_token": this.props.token })
+      body: JSON.stringify({ "authenticity_token": props.token })
     }).then((response) => response.json()
     ).then(
       (json) => {
-        this.setState({
-          like: json.like,
-          likeCount: json.like_count
-        })
+        setLiked(json.like)
+        setLikeCount(json.like_count)
       }
     )
   }
 
-  unLike = () => {
-    fetch(this.props.path, {
+  function unLike() {
+    fetch(props.path, {
       method: 'DELETE',
       headers: new Headers({ "Content-type": "application/json" }),
-      body: JSON.stringify({ "authenticity_token": this.props.token })
+      body: JSON.stringify({ "authenticity_token": props.token })
     }).then((response) => response.json()
     ).then(
       (json) => {
-        this.setState({
-          like: json.like,
-          likeCount: json.like_count
-        })
+        setLiked(json.like)
+        setLikeCount(json.like_count)
       }
     )
   }
 
-  getClass(like) {
-    if (this.state.like) {
+  function getClass(liked) {
+    if (liked) {
       return "like";
     } else {
       return "unlike";
     }
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <a className={this.getClass()} onClick={this.state.like ? this.unLike : this.like} >
-          <span className="glyphicon glyphicon-heart" aria-hidden="true"></span>
-        </a>&nbsp;
-        <a href={this.props.path}>{this.state.likeCount}</a>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <a className={getClass(liked)} onClick={liked ? unLike : like} >
+        <span className="glyphicon glyphicon-heart" aria-hidden="true"></span>
+      </a>&nbsp;
+      <a href={props.path}>{likeCount}</a>
+    </React.Fragment>
+  );
 }
 
 Like.PropTypes = {
