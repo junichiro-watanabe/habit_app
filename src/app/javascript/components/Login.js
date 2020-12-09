126827cef3b822ad15cd9e06bdcc2fcf9c550f6f
Your Name <junichiro_watanabe@watanavi.com>
2020-12-10 01:21:07 +0900

    ログインにフックを使用

--- src/app/javascript/components/Login.js
+++ src/app/javascript/components/Login.js
@@ -16,59 +16,44 @@ const customStyles = {
   }
 };
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function Login(props) {
 
-  afterOpenModal = () => {
-    this.close.style.float = 'right';
-    this.close.style.fontSize = '30px';
-    this.close.style.cursor = 'pointer';
-    this.close.style.marginTop = '5px';
-    this.submit.style.marginTop = '10px';
-    this.guest.style.margin = '10px 0px 20px 0px';
-  }
+  return (
+    <React.Fragment>
+      <Modal
+        isOpen={props.modalIsOpen}
+        onRequestClose={props.closeModal}
+        style={customStyles}
+        className="col-md-4"
+        contentLabel="Login Modal"
+      >
+        <div className="form">
+          <form action="/login" accept-charset="UTF-8" method="post">
+            <input type="hidden" name="authenticity_token" value={props.token} />
+            <span ref={close => close = close} onClick={props.closeModal} id="remove-login" className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
+            <div>
+              <h2>ログイン</h2>
 
-  render() {
-    return (
-      <React.Fragment>
-        <Modal
-          isOpen={this.props.modalIsOpen}
-          onAfterOpen={this.afterOpenModal}
-          onRequestClose={this.props.closeModal}
-          style={customStyles}
-          className="col-md-4"
-          contentLabel="Login Modal"
-        >
-          <div className="form">
-            <form action="/login" accept-charset="UTF-8" method="post">
-              <input type="hidden" name="authenticity_token" value={this.props.token} />
-              <span ref={close => this.close = close} onClick={this.props.closeModal} id="remove-login" className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
-              <div>
-                <h2>ログイン</h2>
-
-                <span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>&nbsp;
+              <span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>&nbsp;
               <label for="session_email">メールアドレス</label>
-                <input className="form-controll" type="email" name="session[email]" id="session_email" />
+              <input className="form-controll" type="email" name="session[email]" id="session_email" />
 
-                <span className="glyphicon glyphicon-eye-close" aria-hidden="true"></span>&nbsp;
+              <span className="glyphicon glyphicon-eye-close" aria-hidden="true"></span>&nbsp;
               <label for="session_password">パスワード</label>
-                <input className="form-controll" type="password" name="session[password]" id="session_password" />
-              </div>
+              <input className="form-controll" type="password" name="session[password]" id="session_password" />
+            </div>
 
-              <div className="col-md-8 col-md-offset-2 submit">
-                <input ref={submit => this.submit = submit} type="submit" name="commit" value="ログイン" className="btn btn-primary" data-disable-with="ログイン" />
-                <a href="/login_guest">
-                  <button ref={guest => this.guest = guest} className="btn btn-secondaly">ゲストユーザでログイン</button>
-                </a>
-              </div>
-            </form>
-          </div>
-        </Modal>
-      </React.Fragment>
-    );
-  }
+            <div className="col-md-8 col-md-offset-2 submit">
+              <input type="submit" name="commit" value="ログイン" className="btn btn-primary" data-disable-with="ログイン" />
+              <a href="/login_guest">
+                <button className="btn btn-secondaly">ゲストユーザでログイン</button>
+              </a>
+            </div>
+          </form>
+        </div>
+      </Modal>
+    </React.Fragment>
+  );
 }
 
 Login.propTypes = {
